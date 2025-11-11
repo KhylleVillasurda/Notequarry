8bd9a85ec2ad7bd4143a897a6575e6dc227f534e
KhylleVillasurda <freakyvader@proton.me>
2025-11-12 01:07:59 +0800

    Updated build.rs
    Fixed MinGW Errors

--- build.rs
+++ build.rs
@@ -1,24 +1,30 @@
-use cpp_build;
+// build.rs
+use std::env;
+use std::path::PathBuf;
 
 fn main() {
-    // Configure Qt build
-    cpp_build::Config::new()
-        .include("/usr/include/qt6")  // Adjust for your Qt installation
-        .include("/usr/include/qt6/QtCore")
-        .include("/usr/include/qt6/QtWidgets")
-        .include("/usr/include/qt6/QtGui")
-        .flag("-std=c++17")
-        .flag("-fPIC")
-        .build("src/main.rs");
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     
-    println!("cargo:rustc-link-search=C:/Qt/6.6.0/mingw_64/lib");
-    println!("cargo:rustc-link-lib=Qt6Core");
-    println!("cargo:rustc-link-lib=Qt6Widgets");
-    println!("cargo:rustc-link-lib=Qt6Gui");
-
-    println!("cargo:rustc-link-search=./build/lib");
-    println!("cargo:rustc-link-lib=notequarry_ui");
+    // UPDATE THIS LINE with your actual Qt Creator build folder!
+    let qt_build_folder = "build\\Desktop_Qt_6_10_0_MinGW_64_bit-Debug\\bin"; // Change this!
+    
+    let qt_build_dir = PathBuf::from(&manifest_dir).join(qt_build_folder);
+    
+    // Tell Cargo where to find the library
+    // Try both debug and release folders
+    println!("cargo:rustc-link-search=native={}/debug", qt_build_dir.display());
+    println!("cargo:rustc-link-search=native={}/release", qt_build_dir.display());
+    println!("cargo:rustc-link-search=native={}", qt_build_dir.display());
+    
+    // Link the library
+    println!("cargo:rustc-link-lib=dylib=notequarry_ui");
+    
+    // Qt libraries
+    println!("cargo:rustc-link-search=C:/Qt/6.10.0/mingw_64/lib");
+    println!("cargo:rustc-link-search=C:/Qt/6.10.0/mingw_64/bin");
     
     println!("cargo:rerun-if-changed=src/ui/mainwindow.h");
     println!("cargo:rerun-if-changed=src/ui/mainwindow.cpp");
+    println!("cargo:rerun-if-changed=src/ui/qt_bridge.h");
+    println!("cargo:rerun-if-changed=src/ui/qt_bridge.cpp");
 }
